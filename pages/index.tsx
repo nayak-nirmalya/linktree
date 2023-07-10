1351b4225e34a50775d944e3b97a784601666b6c
Nirmalya Nayak <n.nayak263@gmail.com>
2023-07-10 15:20:57 +0530

    added avatar image

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,13 @@
 import React from "react";
+import Image from "next/image";
+
+import data from "../data.json";
+import Avatar from "/public/assets/images/Avatar.jpg";
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div className="flex items-center">
+      <Image alt={data.name} src={Avatar} width={48} height={48} />
+    </div>
+  );
 }
