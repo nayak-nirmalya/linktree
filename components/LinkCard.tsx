85dc72c16e7c7b61b8ccc6a7f26b7c7af385ded3
Nirmalya Nayak <n.nayak263@gmail.com>
2023-07-10 19:49:44 +0530

    added few link imgs

--- components/LinkCard.tsx
+++ components/LinkCard.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import React from "react";
 
 interface LinkCardsProps {
@@ -7,5 +8,29 @@ interface LinkCardsProps {
 }
 
 export default function LinkCard({ href, title, image }: LinkCardsProps) {
-  return <div>LinkCard</div>;
+  return (
+    <a
+      href={href}
+      target="_blank"
+      rel="noopener noreferrer"
+      className="flex items-center p-1 w-full rounded-md hover:scale-105 transition-all bg-gray-100 mb-3 max-w-3xl"
+    >
+      <div className="flex text-center w-full">
+        <div className="w-10 h-10">
+          {image && (
+            <Image
+              className="rounded-sm"
+              alt={title}
+              src={image}
+              width={40}
+              height={40}
+            />
+          )}
+        </div>
+        <h2 className="flex justify-center items-center font-semibold w-full text-gray-700 -ml-10">
+          {title}
+        </h2>
+      </div>
+    </a>
+  );
 }
