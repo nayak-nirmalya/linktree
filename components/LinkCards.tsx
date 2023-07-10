bd3baefaf61d9575aad6f99ec79c440e203b932d
Nirmalya Nayak <n.nayak263@gmail.com>
2023-07-10 19:31:39 +0530

    added new component: LinkCards

---
+++ components/LinkCards.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function LinkCards() {
+  return <div>LinkCards</div>;
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,8 +6,15 @@ import Avatar from "/public/assets/images/Avatar.jpg";
 
 export default function Home() {
   return (
-    <div className="flex items-center">
-      <Image alt={data.name} src={Avatar} width={48} height={48} />
+    <div className="flex mx-auto w-full justify-center mt-16 items-center flex-col">
+      <Image
+        className="rounded-full"
+        alt={data.name}
+        src={Avatar}
+        width={96}
+        height={96}
+      />
+      <h1 className="font-semibold mt-4 text-xl">{data.name}</h1>
     </div>
   );
 }
