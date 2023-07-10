036a4ee6721f297d08bba181d36b75798745a2e7
Nirmalya Nayak <n.nayak263@gmail.com>
2023-07-10 20:54:23 +0530

    render social medias

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,8 @@ import Image from "next/image";
 import data from "../data.json";
 
 import LinkCards from "@/components/LinkCards";
+import TwitterIcon from "@/components/Icons/TwitterIcon";
+import GitHubIcon from "@/components/Icons/GitHubIcon";
 
 export default function Home() {
   return (
@@ -17,6 +19,23 @@ export default function Home() {
       />
       <h1 className="font-bold mt-4 mb-8 text-xl text-white">{data.name}</h1>
       <LinkCards links={data.links} />
+      <div className="flex items-center gap-4 mt-8 text-white">
+        {data.socials.map((social) => (
+          <a
+            aria-label={`${social.title} Link`}
+            key={social.href}
+            href={social.href}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {social.href.includes("twitter") ? (
+              <TwitterIcon />
+            ) : social.href.includes("github") ? (
+              <GitHubIcon />
+            ) : null}
+          </a>
+        ))}
+      </div>
     </div>
   );
 }
