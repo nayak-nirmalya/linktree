b92bcfccfa8ee5afc9b8774f7430708858b2e2af
Nirmalya Nayak <n.nayak263@gmail.com>
2024-04-09 08:10:01 +0530

    update import with alias & update browser lists

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import data from "../data.json";
+import data from "@/data.json";
 
 import LinkCards from "@/components/LinkCards";
 import Socials from "@/components/Socials";
