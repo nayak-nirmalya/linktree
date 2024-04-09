4dcd7db54d27357c1cff7755d3aa1efdb0d34e81
Nirmalya Nayak <n.nayak263@gmail.com>
2024-04-09 08:13:38 +0530

    add Open Graph Meta Tags

--- components/MetaTags.tsx
+++ components/MetaTags.tsx
@@ -4,13 +4,22 @@ import Head from "next/head";
 export default function MetaTags() {
   return (
     <Head>
-      <title>LinkTree Clone: Nirmalya Nayak</title>
+      <title>Nirmalya Nayak | Links</title>
       <meta
         name="description"
         content="LinkTree with Next.js + TypeScript + Tailwind CSS"
       />
       <link rel="icon" href="/favicon.ico" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+      <meta property="og:url" content="https://www.nirmalya.info/" />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content="Nirmalya Nayak | Links" />
+      <meta
+        property="og:description"
+        content="LinkTree with Next.js + TypeScript + Tailwind CSS"
+      />
+      <meta property="og:image" content="https://nirmalya.info/profile.jpg" />
     </Head>
   );
 }
